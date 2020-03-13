d858eb5d45e1c0407a9d10c18e0e4d1c624e6d4d
Lmarkk <48249159+Lmarkk@users.noreply.github.com>
2020-03-13 14:29:34 +0200

    Initial commit.

---
+++ src/test/java/fi/tuni/tamk/web_blog_app/WebBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.tuni.tamk.web_blog_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
