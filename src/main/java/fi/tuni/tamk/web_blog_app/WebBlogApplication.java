d0254e5cdc8b9d89f617bce516083f0e56f85d7f
Sorellum <lassi.markkinen@hotmail.com>
2020-03-25 20:16:55 +0200

    Print team name,team member names and curl instructions on backend startup.

--- src/main/java/fi/tuni/tamk/web_blog_app/WebBlogApplication.java
+++ src/main/java/fi/tuni/tamk/web_blog_app/WebBlogApplication.java
@@ -9,6 +9,18 @@ public class WebBlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebBlogApplication.class, args);
+		System.out.println("Team Flammenspitze");
+		System.out.println("Lassi Markkinen, Samu Willman");
+		System.out.println("Curl get all blogposts: curl -X GET http://localhost:8080/blogposts/");
+		System.out.println("Curl post one blogpost: curl -X POST -H \"Content-type: application/json\" -d \"{\"title\": \"test\"," +
+				"\"date\": \"2020.01.01\"," +
+				"\"description\": \"test\"," +
+				"\"likes\": 0," +
+				"\"dislikes\": 0," +
+				"\"liked\": false," +
+				"\"disliked\": false," +
+				"\"content\": \"test\",}\" http://localhost:8080/blogposts/");
+
 	}
 
 }
