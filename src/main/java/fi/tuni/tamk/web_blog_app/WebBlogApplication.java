5276d6a7743334ba113ecd8e32bdad978c44d5e9
Sorellum <lassi.markkinen@hotmail.com>
2020-03-26 18:49:56 +0200

    Update server startup logs.

--- src/main/java/fi/tuni/tamk/web_blog_app/WebBlogApplication.java
+++ src/main/java/fi/tuni/tamk/web_blog_app/WebBlogApplication.java
@@ -1,26 +1,45 @@
 package fi.tuni.tamk.web_blog_app;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebBlogApplication {
+	public static String SERVER = "http://localhost:8080/";
+	public static String URL = "blogposts/";
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebBlogApplication.class, args);
-		System.out.println("Team Flammenspitze");
-		System.out.println("Lassi Markkinen, Samu Willman");
-		System.out.println("Curl get all blogposts: curl -X GET http://localhost:8080/blogposts/");
-		System.out.println("Curl post one blogpost: curl -X POST -H \"Content-type: application/json\" -d \"{\"title\": \"test\"," +
-				"\"date\": \"2020.01.01\"," +
-				"\"description\": \"test\"," +
-				"\"likes\": 0," +
-				"\"dislikes\": 0," +
-				"\"liked\": false," +
-				"\"disliked\": false," +
-				"\"content\": \"test\",}\" http://localhost:8080/blogposts/");
-
 	}
+	@Bean
+	public CommandLineRunner instructions() {
+		return (String... args) -> {
+			Log logger = LogFactory.getLog(WebBlogApplication.class);
+			logger.info("TEAM FLAMMENSPITZE: Lassi Markkinen, Samu Willman");
+			logger.info("------------");
+			logger.info("INSTRUCTIONS");
+			logger.info("------------");
+			logger.info("GET all locations");
+			logger.info("    curl -X GET " + SERVER + URL);
+			// logger.info("GET one location");
+			// logger.info("    curl -X GET " + SERVER + URL + "1");
+			// logger.info("DELETE one location");
+			// logger.info("    curl -X DELETE " + SERVER + URL + "1");
+			logger.info("POST one location");
+			logger.info("    curl -X POST -H \"Content-type: application/json\" -d \"{\"title\": \"test\"," +
+					"\"date\": \"2020.01.01\"," +
+					"\"description\": \"test\"," +
+					"\"likes\": 0," +
+					"\"dislikes\": 0," +
+					"\"liked\": false," +
+					"\"disliked\": false," +
+					"\"content\": \"test\",}\"" + SERVER + URL);
+		};
+	}
+
 
 }
