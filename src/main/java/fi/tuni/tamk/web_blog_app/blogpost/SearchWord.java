95016f1b75ee6b8e80cc5e17ce38d32910a55344
Samu Willman <samukoulu@gmail.com>
2020-04-16 18:39:41 +0300

    Add SearchWord class

---
+++ src/main/java/fi/tuni/tamk/web_blog_app/blogpost/SearchWord.java
@@ -0,0 +1,14 @@
+package fi.tuni.tamk.web_blog_app.blogpost;
+
+public class SearchWord {
+
+    private String searchWord;
+
+    public String getSearchWord() {
+        return searchWord;
+    }
+
+    public void setSearchWord(String searchWord) {
+        this.searchWord = searchWord;
+    }
+}
