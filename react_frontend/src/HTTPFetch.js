9f7122047b85722a98ab29a05a391bbaa4061382
Samu Willman <samukoulu@gmail.com>
2020-04-16 14:43:37 +0300

    Add Fetch for user data

--- react_frontend/src/HTTPFetch.js
+++ react_frontend/src/HTTPFetch.js
@@ -1,12 +1,22 @@
 import React from "react";
+
+
 class HTTPFetch extends React.Component {
 
     constructor(props) {
         super(props);
     }
 
-    fetchDataFromBackend() {
+    fetchBlogDataFromBackend() {
         const apiUrl = `http://localhost:8080/blogposts`;
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => this.setState({post: data}))
+            .catch(err => console.log(err));
+    }
+
+    fetchUserDataFromBackend() {
+        const apiUrl = `http://localhost:8080/users`;
         fetch(apiUrl)
             .then(response => response.json())
             .then(data => this.setState({post: data}))
