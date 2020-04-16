7f5c5210cbf480c44247bf4aed16d4e2530ff892
Samu Willman <samukoulu@gmail.com>
2020-04-16 14:46:51 +0300

    Change elements to material ui elements

--- react_frontend/src/components/layouts/Form.js
+++ react_frontend/src/components/layouts/Form.js
@@ -3,12 +3,24 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import HTTPPost from '../../HTTPPost';
+import TextField from "@material-ui/core/TextField";
+import {makeStyles} from "@material-ui/core/styles";
+import Button from '@material-ui/core/Button';
 
 const httpPost = new HTTPPost();
 const today = new Date();
 const todayDate = today.getDate() + "/"+ parseInt(today.getMonth()+1) +"/"+ today.getFullYear();
 
+const useStyles = makeStyles(theme => ({
+    textArea: {
+        width: 100,
+        height: 150,
+        padding: 1
+    }
+}));
+
 export class Form extends React.Component {
+
 
     constructor(props) {
         super(props);
@@ -18,7 +30,7 @@ export class Form extends React.Component {
 
     handleSubmit(event) {
         console.log('The title is ' + this.titleInput.value);
-        httpPost.postDataToBackend(this.titleInput.value, todayDate, this.descriptionInput.value, this.contentInput.value);
+        httpPost.postBlogDataToBackend(this.titleInput.value, todayDate, this.descriptionInput.value, this.contentInput.value);
         event.preventDefault();
     }
 
@@ -30,33 +42,58 @@ export class Form extends React.Component {
         console.log("Unmounted");
     }
 
-
-
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
             <div style={{maxWidth: 1500, paddingTop: 10, marginTop: 40, marginLeft: 35, marginRight: 35}}>
-                <Card>
-                    <input
+                <Card >
+                    <TextField
+                        id="standard-multiline-flexible"
+                        style={{marginLeft: 35, paddingRight: 80}}
                         name="title"
+                        fullWidth={true}
+                        multiline
+                        label="Title"
                         type="text"
-                        ref={(titleInput) => this.titleInput = titleInput} />
+                        inputRef={(titleInput) => this.titleInput = titleInput} />
+
+                        <TextField
+                        id="standard-multiline-flexible"
+                        style={{marginLeft: 35, paddingRight: 80}}
+                        name="Image URL"
+                        fullWidth={true}
+                        multiline
+                        label="Image URL"
+                        type="text"
+                        inputRef={(descriptionInput) => this.descriptionInput = descriptionInput} />
+
                     <CardHeader style={{textAlign: 'center'}}
-                               // subheader={todayDate}
                     />
+
                     <CardContent>
-                        <input
+                        <TextField
+                            id="standard-multiline-flexible"
+                            style={{marginLeft: 20, paddingRight: 50}}
                             name="description"
+                            fullWidth={true}
+                            multiline
+                            label="Description"
                             type="text"
-                            ref={(descriptionInput) => this.descriptionInput = descriptionInput} />
+                            inputRef={(descriptionInput) => this.descriptionInput = descriptionInput} />
                     </CardContent>
                         <CardContent>
-                            <input
+                            <TextField
+                                id="standard-multiline-flexible"
+                                style={{marginLeft: 20, paddingRight: 50}}
                                 name="content"
+                                label="Content"
+                                fullWidth={true}
+                                multiline
                                 type="text"
-                                ref={(contentInput) => this.contentInput = contentInput} />
-
-                            <input type="submit" value="Submit" />
+                                inputRef={(contentInput) => this.contentInput = contentInput} />
+                            <div style={{textAlign: 'center', marginTop: 30}}>
+                            <Button type="submit" value="submit" variant="contained">Submit</Button>
+                            </div>
                         </CardContent>
                 </Card>
             </div>
