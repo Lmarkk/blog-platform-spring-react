be51be8ea3d193588b27f38fc55a9c0480bee322
Samu Willman <samukoulu@gmail.com>
2020-05-07 00:42:29 +0300

    Add loader

--- react_frontend/src/components/layouts/Panes.js
+++ react_frontend/src/components/layouts/Panes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import LeftPane from "./LeftPane";
 import RightPane from "./RightPane";
+import Loader from "react-loader-spinner";
 
 const styles = {
     PaperRight: {
@@ -18,13 +19,21 @@ export default function Panes() {
             <Grid container spacing={0}>
                 <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
                     <LeftPane style={styles.PaperLeft}/>
+                    <div className='loader'>
+                    <Loader
+                        type={"Puff"}
+                        color={"#00BFFF"}
+                        height={200}
+                        width={200}
+                        timeout={6000}
+                    />
+                    </div>
                 </Grid>
                 <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
                     <RightPane style={styles.PaperRight}/>
                 </Grid>
             </Grid>
         )
-
 }
 
 
