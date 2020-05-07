070b99204937944a0db5f1e943171315fce5cb11
Samu Willman <samukoulu@gmail.com>
2020-05-07 20:38:58 +0300

    Add static files

---
+++ src/main/resources/static/precache-manifest.0186cfaab5980079a8fcc79619852919.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3a3590fd11728da30614ad13c4a071",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3550d2b6ea56f8962253",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a22d18f58d9702bbe51e",
+    "url": "/static/js/2.23823969.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.23823969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3550d2b6ea56f8962253",
+    "url": "/static/js/main.4e61ceee.chunk.js"
+  },
+  {
+    "revision": "13c1efbaeb676adde1da",
+    "url": "/static/js/runtime-main.77ccd6e2.js"
+  },
+  {
+    "revision": "1fae6530175beea967cd235a040532ee",
+    "url": "/static/media/panorama.1fae6530.jpg"
+  }
+]);
